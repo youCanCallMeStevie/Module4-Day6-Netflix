49f9631599d641b6877c4d658c49cc8e94d10ac0
Ermal-code <72197798+Ermal-code@users.noreply.github.com>
2020-11-20 11:58:55 +0100

    added movies

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import "./App.css";
+import MovieList from "./components/MovieList";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <MovieList />
+    </div>
+  );
 }
 
 export default App;
