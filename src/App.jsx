1c5a4056e2966747bb50cba585b1f93761ce2736
Manuel Desole <46155957+madnol@users.noreply.github.com>
2020-11-20 11:05:08 +0100

    first things

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import "./App.css";
-
+import "./components/JumboCarousel";
+import JumboCarousel from "./components/JumboCarousel";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <JumboCarousel />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/JumboCarousel.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import {} from "react-bootstrap";
+
+class JumboCarousel extends React.Component {
+  render() {
+    return <div>Ciao</div>;
+  }
+}
+
+export default JumboCarousel;
