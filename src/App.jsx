f0b797ffeeaec02a4a6ba7a18332defff246221d
youCanCallMeStevie <72197493+youCanCallMeStevie@users.noreply.github.com>
2020-11-20 11:46:19 +0000

    footer added

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,17 @@ import "bootstrap/dist/css/bootstrap.min.css";
 // import "font-awesome/css/font-awesome.css";
 import React from "react";
 import "./App.css";
-
 import JumboCarousel from "./components/JumboCarousel";
-
-    
 import NavBar from "./components/NavBar";
 import MovieList from "./components/MovieList";
+import Footer from "./components/Footer";
 function App() {
   return (
     <div className="App">
       <NavBar />
-       <JumboCarousel />
+      <JumboCarousel />
       <MovieList />
+      <Footer />
 
     </div>
   );
